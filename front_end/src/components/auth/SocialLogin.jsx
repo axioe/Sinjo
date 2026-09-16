42ff672d76e2a2c05b6b1a38d403cba4c39b16c1
minxxnn <mquxse@gmail.com>
2026-09-16 12:39:09 +0900

    commit?

--- front_end/src/components/auth/SocialLogin.jsx
+++ front_end/src/components/auth/SocialLogin.jsx
@@ -9,8 +9,8 @@ function SocialLogin({mode = "login"}) {
   }
 
   const handleGoogle = () => {
-    alert("구글 로그인 준비 중 입니다.")
-  }
+  window.location.href = apiUrl("/api/auth/google");
+};
 
   return (
     <div className="social-login">
